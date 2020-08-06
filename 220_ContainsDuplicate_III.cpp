c3b7c75ae2498927b2089e59b4b94bbb45cea8d1
Mohit Singh <30595867+qmohitsingh@users.noreply.github.com>
2020-08-06 23:05:56 +0530

    Update 220_ContainsDuplicate_III.cpp

--- 220_ContainsDuplicate_III.cpp
+++ 220_ContainsDuplicate_III.cpp
@@ -18,26 +18,23 @@ class Solution {
 public:
     bool containsNearbyAlmostDuplicate(vector<int>& nums, int k, int t) {
         
-        if(nums.size() == 0) return false;
+        if (nums.size() <= 1) { return false; }
         
-        if(k==0) return false;
+        multiset<long long> aSet;
+        multiset<long long>::iterator aIter;
         
-        multiset<int> bst;
-        for(int i=0; i < nums.size(); i++)
-        {
-            const auto it = bst.lower_bound(nums[i] - t);
-            if(it != bst.end() && abs(*it - nums[i]) <= t)
-                return true;
-                
-            bst.insert(nums[i]);
-            
-            if(bst.size() > k)
-                bst.erase( bst.find(nums[i-k]) );
+        for (int i=0;i<nums.size();i++) {
+            if (i > k) { aSet.erase(nums[i - k - 1]); }
+            aIter = aSet.lower_bound((long long)nums[i] - (long long)t);
+            if (aIter != aSet.end()) {
+                if (abs((long long)nums[i] - *aIter) <= (long long)t) {
+                    return true;
+                }
+            }
+            aSet.insert((long long)nums[i]);   
         }
-        
         return false;
     }
 
 };
-
 
