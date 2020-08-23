5e4e0357ec35bac78a0e54976ef9ebb2dbd7dae8
Mohit Singh <30595867+qmohitsingh@users.noreply.github.com>
2020-08-23 23:56:46 +0530

    Update 414_ThirdMaximumNumber.cpp

--- 414_ThirdMaximumNumber.cpp
+++ 414_ThirdMaximumNumber.cpp
@@ -1,1 +1,25 @@
 
+
+class Solution {
+public:
+    int thirdMax(vector<int>& nums) {
+        
+        set<int> s; 
+        for(int i = 0; i < nums.size(); i++) { 
+            s.insert(nums[i]);
+        }
+        
+        vector<int> temp;  
+        for(auto i : s) {  
+            temp.push_back(i);
+        }  
+        if (temp.size() == 1) {
+            return temp[0];
+        }
+        if (temp.size() == 2) {
+            return max(temp[0], temp[1]);
+        } else {
+            return temp[temp.size()-3]; 
+        }
+    }
+};
